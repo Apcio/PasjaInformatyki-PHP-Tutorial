a834658147f2f854b596a2a43b81c0f80c1b79a1
Apcio <tomasz.m2@gmail.com>
2018-11-21 22:43:53 +0100

    Rozpoczęcie części 5 kursu

---
+++ PHP5/config.php
@@ -0,0 +1,9 @@
+<?php
+    return
+        [
+            'host' => 'localhost',
+            'user' => 'phpmyadmin',
+            'password' => 'zaq!@WSX1',
+            'database' => 'newsletter'
+        ];
+?>
